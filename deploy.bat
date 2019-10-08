689f520a509d698f798b3625b50bd4b1d488f35f
eyashin@zymergen.com <eyashin@umich.edu>
2019-10-07 18:25:23 -0700

    fixed resume

--- deploy.bat
+++ deploy.bat
@@ -3,6 +3,6 @@ echo off
 CALL workon ed
 CALL python scripts/projectpages.py
 CALL git add *
-set /P cmtMsg ="Deploy commit message: "
+set /P cmtMsg="Deploy commit message: "
 CALL git commit -m "%cmtMsg%"
 CALL git push 
